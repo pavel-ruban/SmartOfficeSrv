cf9003a5b6411db525e6abb072837d7cac9f62f3
Roman Samarin <job.samarin.r@gmail.com>
2016-09-30 19:02:39 +0700

    refs #40169 async server core implemented

---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include "smartoffice-srv.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <boost/asio.hpp>
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+using boost::asio::ip::tcp;
+
+class session
+        : public std::enable_shared_from_this<session>
+{
+public:
+    session(tcp::socket socket)
+            : socket_(std::move(socket))
+    {
+    }
+
+    void start()
+    {
+        do_read();
+    }
+
+private:
+    std::string *str = new std::string("ALLO");
+    void do_read()
+    {
+        auto self(shared_from_this());
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+                                [this, self](boost::system::error_code ec, std::size_t length) {
+                                    if (!ec) {
+                                        do_write(length);
+                                    }
+                                });
+    }
+
+    void do_write(std::size_t length)
+    {
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_, boost::asio::buffer(biba_, length),
+                                 [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                                     if (!ec) {
+                                         do_read();
+                                     }
+                                 });
+    }
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char biba_[5] = {"biba"};
+    char data_[max_length];
+};
+
+class server
+{
+public:
+    server(boost::asio::io_service& io_service, short port)
+            : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
+              socket_(io_service) {
+        do_accept();
+    }
+
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(socket_,
+                               [this](boost::system::error_code ec) {
+                                   if (!ec) {
+                                       std::make_shared<session>(std::move(socket_))->start();
+                                   }
+
+                                   do_accept();
+                               });
+    }
+
+    tcp::acceptor acceptor_;
+    tcp::socket socket_;
+};
+
+int main(int argc, char* argv[])
+{
+
+    try {
+        boost::property_tree::ptree pt;
+        boost::property_tree::ini_parser::read_ini("../include/config", pt);
+
+
+
+        boost::asio::io_service io_service;
+        short port;
+        std::istringstream (pt.get<std::string>("General.port")) >> port;
+
+        server s(io_service, port);
+
+        io_service.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
